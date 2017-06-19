551cd1c5df3c58c2853cde06ffc33db96a16a0f2
baichechekei <baichechekeii@gmail.com>
2017-06-19 23:24:50 +0900

    Initialize repo

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :comments
+  get 'pages/index'
+
+  resources :projects
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: redirect('/projects')
+end
